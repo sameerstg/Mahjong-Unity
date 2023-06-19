229aab1fd104582c02d9b78ac9e0664aac702f51
unknown <sameerstg@outlook.com>
2023-06-19 05:58:41 -0700

    selection tile confition done

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -17,13 +17,19 @@ public class BoardManager : MonoBehaviour
     public GameObject piecesParent;
     public Material meshMaterial;
     public Material selectedMeshMaterial;
-    
+    Vector3 size;
+    GameObject Cube;
+    public List<TileInfo> clickable = new();
+
     private void Awake()
     {
         _instance = this;
     }
     private void Start()
     {
+        Cube = piecesParent.transform.GetChild(0).gameObject;
+
+        size = Cube.GetComponent<Renderer>().bounds.size;
 
         //table = new Table(layers, columns, rows);
         //grid = new (x, y, layers);
@@ -37,14 +43,23 @@ public class BoardManager : MonoBehaviour
 
 
 
-       /* if (Mathf.Pow(columns * rows, layers) % 4 != 0)
+        /* if (Mathf.Pow(columns * rows, layers) % 4 != 0)
+         {
+             return;
+         }*/
+        int sum = 0;
+        foreach (var item in grid.depth)
         {
+            foreach (var item2 in item.columns)
+            {
+                sum += item2.tiles.Length;
+            }
+        }
+        if (sum % 2 != 0)
+        {
+            Debug.LogError("generation not possible");
             return;
-        }*/
-        GameObject Cube = piecesParent.transform.GetChild(0).gameObject;
-
-
-        Vector3 size = Cube.GetComponent<Renderer>().bounds.size;
+        }
         int id = 0;
 
         for (int i = 0; i < grid.depth.Length; i++)
@@ -55,70 +70,165 @@ public class BoardManager : MonoBehaviour
 
                 for (int k = 0; k < grid.depth[i].columns[j].tiles.Length; k++)
                 {
-                    
-                                       Vector3 firstTrans = new Vector3();
-                                       firstTrans.x = (((grid.depth[i].columns.Length - 1) * (-size.x)) / 2) + ((size.x+gap) * j);
-                    firstTrans.y = ((grid.depth[i].columns[j].tiles.Length - 1) * (size.y) / 2) + ((-size.y-gap) * k);
-                    firstTrans.z = (((grid.depth.Length - 1) * (-size.z )/2)+ ((size.z+gap)*i));
-                    int matchId = Random.Range(0, piecesParent.transform.childCount - 1);
-                    GameObject tile = Instantiate(piecesParent.transform.GetChild(matchId).gameObject, firstTrans, Quaternion.identity,transform.GetChild(0));
+
+                    Vector3 firstTrans = GetWorldPostion(k, j, i);
+                    int matchId = GetRandomMatchId();
+                    GameObject tile = Instantiate(piecesParent.transform.GetChild(matchId).gameObject, firstTrans, Quaternion.identity, transform.GetChild(0));
                     tile.name = $"x = {k} , y = {j} , z = {i}";
                     Tile tileScript = tile.AddComponent<Tile>();
+
                     
-                    tileScript.tileInfo = new TileInfo(k, j, i,id,matchId);
-                    id++;
+                    tileScript.tileInfo = new TileInfo(k, j, i, id, matchId,tile);
+                    grid.depth[i].columns[j].tiles[k] = tileScript.tileInfo;
+                   id++;
                     tile.AddComponent<BoxCollider>();
-                                   }
+                }
             }
         }
-            }
-   
-    
-    bool CanSelect(Tile tile)
-    {
-        try
-        {
-            if (GetTileByCoord(tile.tileInfo.x, tile.tileInfo.y - 1, tile.tileInfo.layer) == null)
-            {
-                return true;
-
-            }
-            if (GetTileByCoord(tile.tileInfo.x, tile.tileInfo.y + 1, tile.tileInfo.layer) == null)
-            {
-                return true;
-            }
-        }
-        catch (System.Exception)
-        {
-
-            return true;    
-        }
-        return false;
         
     }
-       public void TileSelected(Tile tile)
+    int GetRandomMatchId()
     {
-        if (!CanSelect(tile))
+        return Random.Range(0, piecesParent.transform.childCount - 1);
+    }
+    [ContextMenu("GetAllClickable")]
+    public void GetAllClickable()
+    {
+        clickable.Clear();
+        foreach (var d in grid.depth)
+        {
+            foreach (var y in d.columns)
+            {
+                foreach (var x in y.tiles)
+                {
+                    if (CanSelect(x))
+                    {
+                        clickable.Add(x);
+                    }
+                    else
+                    {
+                        Debug.Log("cant");
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="k">x</param>
+    /// <param name="j">y</param>
+    /// <param name="i">layer/depth</param>
+    /// <returns></returns>
+    Vector3 GetWorldPostion(int k, int j, int i)
+    {
+        Vector3 firstTrans = new Vector3();
+        firstTrans.x = (((grid.depth[i].columns.Length - 1) * (-size.x)) / 2) + ((size.x + gap) * j);
+        firstTrans.y = ((grid.depth[i].columns[j].tiles.Length - 1) * (size.y) / 2) + ((-size.y - gap) * k);
+        firstTrans.z = (((grid.depth.Length - 1) * (-size.z) / 2) + ((size.z + gap) * i));
+        return firstTrans;
+    }
+    int GetTileXCount(int y, int depth)
+    {
+        return grid.depth[depth].columns[y].tiles.Length;
+    }
+    int GetTileYCount( int depth)
+    {
+        return grid.depth[depth].columns.Length;
+    }
+    bool CanSelect(TileInfo tileInfo)
+    {
+        if (tileInfo.y-1>=0)
+        {
+            if (GetTileXCount(tileInfo.y, tileInfo.layer) % 2 == GetTileXCount(tileInfo.y - 1, tileInfo.layer)
+                        % 2)
+            {
+                print("here");
+
+                if (GetTileInfoByCoord(tileInfo.x, tileInfo.y - 1, tileInfo.layer) == null)
+                {
+
+                    print("here");
+                    return true;
+                }
+
+            }
+            else
+            {
+                print("here");
+
+                              
+                if ((tileInfo.x + 1>=GetTileXCount(tileInfo.y - 1, tileInfo.layer)|| GetTileInfoByCoord(tileInfo.x + 1, tileInfo.y - 1, tileInfo.layer) == null )
+                    && (GetTileInfoByCoord(tileInfo.x - 1, tileInfo.y - 1, tileInfo.layer) == null || tileInfo.y <0))
+                {
+                    print("here");
+
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            return true;
+        }
+        if (tileInfo.y+1<GetTileYCount(tileInfo.layer))
+        {
+            if (GetTileXCount(tileInfo.y, tileInfo.layer) % 2 == GetTileXCount(tileInfo.y + 1, tileInfo.layer)
+                        % 2)
+            {
+                print("here");
+
+                if (GetTileInfoByCoord(tileInfo.x, tileInfo.y + 1, tileInfo.layer) == null)
+                {
+                    print("here");
+
+                    return true;
+                }
+            }
+            else
+            {
+                print("here");
+
+                if ((tileInfo.x + 1 >= GetTileXCount(tileInfo.y + 1, tileInfo.layer)||GetTileInfoByCoord(tileInfo.x + 1, tileInfo.y + 1, tileInfo.layer) == null )
+                    && (tileInfo.x < 0||GetTileInfoByCoord(tileInfo.x - 1, tileInfo.y + 1, tileInfo.layer) == null ))
+                {
+                    print("here");
+
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            return true;
+        }
+        
+        
+        return false;
+
+    }
+    public void TileSelected(Tile tile)
+    {
+        if (!CanSelect(tile.tileInfo))
         {
             return;
         }
-        Debug.Log(tile.tileInfo.matchId);
-        
+
         if (prevSelectedTile == null)
         {
-            
+
             prevSelectedTile = tile;
-                       //mats[2] = selectedMeshMaterial;
+            //mats[2] = selectedMeshMaterial;
             foreach (var item in tile.GetComponent<MeshRenderer>().materials)
             {
                 item.color = Color.yellow;
             }
-                        
+
             return;
         }
         if (tile.tileInfo.matchId == prevSelectedTile.tileInfo.matchId && prevSelectedTile.tileInfo.id != tile.tileInfo.id)
         {
-            Matched(prevSelectedTile,tile);
+            Matched(prevSelectedTile, tile);
         }
         else
         {
@@ -126,41 +236,51 @@ public class BoardManager : MonoBehaviour
             foreach (var item in tile.GetComponent<MeshRenderer>().materials)
             {
                 item.color = Color.yellow;
-            }foreach (var item in prevSelectedTile.GetComponent<MeshRenderer>().materials)
+            }
+            foreach (var item in prevSelectedTile.GetComponent<MeshRenderer>().materials)
             {
                 item.color = Color.white;
             }
             prevSelectedTile = tile;
         }
     }
-    void Matched(Tile tile1,Tile tile2)
+    void Matched(Tile tile1, Tile tile2)
     {
-        
-               prevSelectedTile = null;
+
+        prevSelectedTile = null;
         Destroy(tile1.gameObject);
         Destroy(tile2.gameObject);
-    }
-    TileInfo GetTileByCoord(int x,int y,int depth)
-    {
-        try
+        for (int i = 0; i < grid.depth.Length; i++)
         {
-            return grid.depth[depth].columns[x].tiles[y];
+            for (int j  = 0; j< grid.depth[i].columns.Length; j++)
+            {
+                for (int k = 0; k < grid.depth[i].columns[j].tiles.Length; k++)
+                {
+                    if (grid.depth[i].columns[j].tiles[j] == tile1.tileInfo|| grid.depth[i].columns[j].tiles[j] == tile2.tileInfo)
+                    {
+                        grid.depth[i].columns[j].tiles[j].go = null;
+                    }
+                    
+                }
+            }
         }
-        catch (System.Exception)
-        {
+           }
+    GameObject GetTileInfoByCoord(int x, int y, int depth)
+    {
+        Debug.Log($"{x},{y},{depth}");
+        Debug.Log(grid.depth[depth].columns[y].tiles[x].go);
+        return grid.depth[depth].columns[y].tiles[x].go;
 
-            return null;        }
-        
-    }
-}
+    } }
 
 
 [System.Serializable]
 public class X
 {
-    public X(){
+    public X()
+    {
     }
-   [field: SerializeField] public GameObject tile;
+    [field: SerializeField] public GameObject tile;
 }
 [System.Serializable]
 
@@ -174,14 +294,14 @@ public class Y
             x[i] = new X();
         }
     }
-   [field: SerializeField] public X[] x;
+    [field: SerializeField] public X[] x;
 }
 [System.Serializable]
 
 public class Layer
 {
 
-    public Layer(int yAmount,int xAmount)
+    public Layer(int yAmount, int xAmount)
     {
         y = new Y[yAmount];
         for (int i = 0; i < yAmount; i++)
@@ -190,13 +310,13 @@ public class Layer
         }
     }
 
-   [field: SerializeField]public Y[] y;
+    [field: SerializeField] public Y[] y;
 }
 [System.Serializable]
 public class Table
 {
-    [field:SerializeField]public Layer[] layers;
-    public Table(int layerAmount,int yAmount,int xAmount)
+    [field: SerializeField] public Layer[] layers;
+    public Table(int layerAmount, int yAmount, int xAmount)
     {
         layers = new Layer[layerAmount];
         for (int i = 0; i < layerAmount; i++)
@@ -208,34 +328,36 @@ public class Table
 [System.Serializable]
 public class TileInfo
 {
-    [field:SerializeField]public int id;
-    [field:SerializeField]public int matchId;
-    [field:SerializeField]public int x;
-    [field:SerializeField]public int y;
-    [field:SerializeField]public int layer;
+    [field: SerializeField] public int id;
+    [field: SerializeField] public int matchId;
+    [field: SerializeField] public int x;
+    [field: SerializeField] public int y;
+    [field: SerializeField] public int layer;
+    [field: SerializeField] public GameObject go;
     public Piece pieceType;
-    public void SetIds(int id,int matchId)
+    public void SetIds(int id, int matchId)
     {
         this.id = id;
         this.matchId = matchId;
     }
-    public TileInfo( int x, int y,int layer,int id,int matchId)
+    public TileInfo(int x, int y, int layer, int id, int matchId,GameObject go)
     {
         this.x = x;
         this.y = y;
         this.layer = layer;
         this.matchId = matchId;
         this.id = id;
+        this.go = go;
     }
 }
 [System.Serializable]
 public class Grid
 {
     public Depth[] depth;
-    
+
 
 }
-[System.Serializable ]
+[System.Serializable]
 public class Depth
 {
     public Column[] columns;
@@ -247,5 +369,5 @@ public class Column
 }
 public enum Piece
 {
-    none,A
+    none, A
 }
